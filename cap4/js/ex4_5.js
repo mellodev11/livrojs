f965cf2d25961906f59a42ed03eaa8cd3ade1240
mellodev11 <robertale45@gmail.com>
2024-07-06 12:10:01 -0300

    finalizando ex 4_5

--- cap4/js/ex4_5.js
+++ cap4/js/ex4_5.js
@@ -4,5 +4,11 @@ const resp = document.querySelector("h2")
 frm.addEventListener("submit", (e) =>{
     e.preventDefault()
     const numero = Number(frm.inNumber.value)
-    
+    const raiz  = Math.sqrt(numero)
+
+    if(Number.isInteger(raiz)){
+        resp.innerText = `Raiz: ${raiz}`
+    }else{
+        resp.innerText = `Não há raiz exata para o ${numero}`
+    }
 })
